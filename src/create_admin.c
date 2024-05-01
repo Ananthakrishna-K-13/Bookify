6d6d07c1b8d52e862752b03fea2eada513ce40b0
ANanthakrishna K <Ananthakrishna.K@iiitb.ac.in>
2024-05-01 11:24:31 +0530

    initial

---
+++ src/create_admin.c
@@ -0,0 +1,20 @@
+#include "../header/head.h"
+
+int main()
+{
+    char buff[100];
+    memset(buff,0,sizeof(buff));
+    printf("Enter username: => ");
+    scanf("%s",buff);
+    struct User u;
+    strcpy(u.username,buff);
+    printf("Enter password: => ");
+    memset(buff,0,sizeof(buff));
+    scanf("%s",buff);
+    strcpy(u.password,buff);
+    u.type=0;
+    int fd=open("../db/users.bin",O_CREAT|O_RDWR,0666);
+    lseek(fd,0,SEEK_END);
+    write(fd,&u,sizeof(u));
+    close(fd);
+}
